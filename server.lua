5e3ddaee774575efc612e620efda45406d17d8ca
HackerGeo <42976769+HackerGeo-sp1ne@users.noreply.github.com>
2021-01-16 19:48:08 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -24,6 +24,8 @@ MySQL = module("vrp_mysql", "MySQL")
 MySQL.createCommand("vRP/create_account", "INSERT IGNORE INTO vrp_login(user_id, username, parola) VALUES(@user_id, @username, @parola)")
 MySQL.createCommand("vRP/get_account", "SELECT * FROM vrp_login WHERE user_id = @user_id")
 
+local characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.:_!#$()[]{}<>'"
+
 AddEventHandler("vRP:playerSpawn",function(source,first_spawn)
   if first_spawn then
     TriggerClientEvent("login:open", source)
@@ -48,29 +50,54 @@ AddEventHandler('login:check', function(username,parola)
   end)
 end)
 
+--newpass oldpass newpass
+RegisterCommand("newpass", function(player, args)
+  local user_id = vRP.getUserId({player})
+  if args[1] ~= nil and args[2] ~= nil then
+    MySQL.query("vRP/get_account", {user_id = user_id}, function(rows, affected)
+      if #rows > 0 then
+        if (rows[1].parola == args[1]) then
+          vRPclient.notify(player,{"~g~Parola ta a fost schimbata cu succes!"})
+          MySQL.query("vRP/change_password", {user_id = user_id, username = rows[1].username, parola = args[2]})
+        else
+          vRPclient.notify(player,{"~g~Parola ta nu corespunde cu cea din server!"})
+        end
+      else
+        vRPclient.notify(player,{"~r~Nu ai creat nici-un cont!"})
+      end
+    end)
+  end
+end)
+
 RegisterServerEvent('register:create')
 AddEventHandler('register:create', function(username,parola,parola2)
 	local user_id = vRP.getUserId({source})
   local player = vRP.getUserSource({user_id})
-  if (parola == parola2) then
-    if(string.len(parola) >= 5) and (string.len(parola) <= 10) then
-      if not (username == parola) then
-        MySQL.query("vRP/get_account", {user_id = user_id}, function(rows, affected)
-          if #rows == 0 then
-            vRPclient.notify(player,{"~g~Ai registrat contul cu succes!"})
-            vRPclient.notify(player,{"~b~USERNAME: ~w~"..username..", ~b~PAROLA: ~w~"..parola})
-            MySQL.query("vRP/create_account", {user_id = user_id, username = username, parola = parola})
-          else
-            vRPclient.notify(player,{"~r~Ai deja un cont! ~w~("..rows[1].username..")"})
-          end
-        end)
+  local user = sanitizeString(username, characters, true)
+  local pass = sanitizeString(parola, characters, true)
+  if user == username and pass == parola then
+    if (parola == parola2) then
+      if(string.len(parola) >= 5) and (string.len(parola) <= 15) then
+        if not (username == parola) then
+          MySQL.query("vRP/get_account", {user_id = user_id}, function(rows, affected)
+            if #rows == 0 then
+              vRPclient.notify(player,{"~g~Ai registrat contul cu succes!"})
+              vRPclient.notify(player,{"~b~USERNAME: ~w~"..username..", ~b~PAROLA: ~w~"..parola})
+              MySQL.query("vRP/create_account", {user_id = user_id, username = username, parola = parola})
+            else
+              vRPclient.notify(player,{"~r~Ai deja un cont! ~w~("..rows[1].username..")"})
+            end
+          end)
+        else
+          vRPclient.notify(player,{"~r~Parola trebuie sa fie diferita de username!"})
+        end
       else
-        vRPclient.notify(player,{"~r~Parola trebuie sa fie diferita de username!"})
+        vRPclient.notify(player,{"~r~Parola trebuie sa contina intre 5 si 15 caractere!"})
       end
     else
-      vRPclient.notify(player,{"~r~Parola trebuie sa contina intre 5 si 10 caractere!"})
+      vRPclient.notify(player,{"~r~Parolele nu corespund!"})
     end
   else
-    vRPclient.notify(player,{"~r~Parolele nu corespund!"})
+    vRPclient.notify(player,{"~r~Au fost gasite caractere interzise in nume sau parola"})
   end
 end)
